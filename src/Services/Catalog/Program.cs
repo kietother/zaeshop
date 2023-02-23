652ffe5a01ee467edb5419364e14b255af33e707
kietdev1 <124799849+kietdev1@users.noreply.github.com>
2023-02-23 04:53:09 +0000

    Init Project

---
+++ src/Services/Catalog/Program.cs
@@ -0,0 +1,19 @@
+using Catalog.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Additional configuration is required to successfully run gRPC on macOS.
+// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+
+// Add services to the container.
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddGrpc();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapGrpcService<GreeterService>();
+
+app.Run();
