80f8aff7108dea42d58acf78099cbdf69ec503af
kietdev1 <124799849+kietdev1@users.noreply.github.com>
2023-03-13 09:25:57 +0000

    Update code

--- src/Services/Identity/Identity.Infrastructure/AppIdentityDbContext.cs
+++ src/Services/Identity/Identity.Infrastructure/AppIdentityDbContext.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Identity.Domain.AggregatesModel.UserAggregate;
 using Identity.Infrastructure.EntityConfigurations;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
@@ -17,9 +18,15 @@ namespace Identity.Infrastructure
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
 
             builder.ApplyConfiguration(new UserEntityTypeConfiguration());
             builder.ApplyConfiguration(new UserTokenEntityTypeConfiguration());
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Server=tiny.db.elephantsql.com; Port=5432; User Id=vhbvlhko; Password=6GiIE2wvPo-Rln1EA6tcbfNE0Tadk8wX; Database=vhbvlhko");
         }
     }
 }
