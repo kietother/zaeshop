ee04c3829da9e5b4274510cd79b9183315962704
kietdev1 <124799849+kietdev1@users.noreply.github.com>
2023-04-23 08:38:14 +0000

    Add-Migration

--- src/Services/Identity/Identity.Infrastructure/Migrations/20230423082851_UpdateUserTokenTable.Designer.cs
+++ src/Services/Identity/Identity.Infrastructure/Migrations/20230423082851_UpdateUserTokenTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Identity.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Identity.Infrastructure.Migrations
 {
     [DbContext(typeof(AppIdentityDbContext))]
-    partial class AppIdentityDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230423082851_UpdateUserTokenTable")]
+    partial class UpdateUserTokenTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
