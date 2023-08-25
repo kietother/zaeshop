9598cc33ed5ea1de01c60df008d1bd84332229d8
kietdev1 <124799849+kietdev1@users.noreply.github.com>
2023-08-25 13:21:05 +0700

    Update User_All_Paging.sql

--- src/Services/Identity/Identity.API/App_Data/StoredProcedures/User_All_Paging.sql
+++ src/Services/Identity/Identity.API/App_Data/StoredProcedures/User_All_Paging.sql
@@ -1,56 +1,49 @@
 CREATE OR ALTER PROCEDURE User_All_Paging
-	@pageNumber INT,
-	@pageSize INT
+  @pageNumber INT,
+  @pageSize	  INT
 AS
 BEGIN
-	SET NOCOUNT ON;
+SET NOCOUNT ON;
 
-	-- Validate parameters
-	IF @pageNumber <= 0
-		SET @pageNumber = 1;
+-- Validate parameters
 
-	IF @pageSize <= 0
-		SET @pageSize = 10; -- Default page size
+IF @pageNumber<=0
+	SET @pageNumber=1;
 
-	DECLARE @offset INT = (@pageNumber - 1) * @pageSize;
+IF @pageSize<=0
+	SET @pageSize=10; -- Default page size
 
-	;
-	WITH FilteredData
-	AS
-	(SELECT
-			ROW_NUMBER() OVER (ORDER BY u.Id) AS RowNum
-		   ,u.Id
-		   ,u.FullName
-		   ,u.UserName
-		   ,u.Email
-		   ,u.EmailConfirmed
-		   ,u.CreatedOnUtc
-		   ,u.UpdatedOnUtc
-		FROM [User] u
-	),
-	TotalRecords
-	AS
-	(SELECT
-			COUNT_BIG(1) AS RowNum
-		FROM FilteredData),
-	PagingRecord
-	AS
-	(SELECT
-			*
-		FROM FilteredData
-		WHERE RowNum BETWEEN @offset + 1 AND @offset + @pageSize)
-	SELECT
-		RowNum
-	   ,NULL AS Id
-	   ,NULL AS FullName
-	   ,NULL AS UserName
-	   ,NULL AS Email
-	   ,NULL AS EmailConfirmed
-	   ,NULL AS CreatedOnUtc
-	   ,NULL AS UpdatedOnUtc
-	FROM TotalRecords
-	UNION
-	SELECT
-		*
-	FROM PagingRecord
+DECLARE
+  @offset INT=(@pageNumber-1)*@pageSize;
+
+;
+WITH FilteredData
+	 AS (SELECT ROW_NUMBER( ) OVER (ORDER BY u.Id) AS RowNum,
+				u.Id,
+				u.FullName,
+				u.UserName,
+				u.Email,
+				u.EmailConfirmed,
+				u.CreatedOnUtc,
+				u.UpdatedOnUtc
+		   FROM [User] u),
+	 TotalRecords
+	 AS (SELECT COUNT_BIG( 1 ) AS RowNum
+		   FROM FilteredData),
+	 PagingRecord
+	 AS (SELECT*
+		   FROM FilteredData
+		   WHERE RowNum BETWEEN @offset+1 AND @offset+@pageSize)
+SELECT RowNum,
+	   NULL AS Id,
+	   NULL AS FullName,
+	   NULL AS UserName,
+	   NULL AS Email,
+	   NULL AS EmailConfirmed,
+	   NULL AS CreatedOnUtc,
+	   NULL AS UpdatedOnUtc
+FROM TotalRecords
+UNION
+SELECT*
+FROM PagingRecord
 END
