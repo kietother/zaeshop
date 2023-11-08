3d6d15630050b0f3a9a184e9d038abbb63244d7b
kietdev1 <124799849+kietdev1@users.noreply.github.com>
2023-11-08 03:16:25 +0000

    Fix docker-compose build a long time

--- src/Services/HangFireServer/Program.cs
+++ src/Services/HangFireServer/Program.cs
@@ -9,7 +9,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 builder.Services.AddHangFireServices(builder.Configuration);
 
 // Hangfire will DI of Portal to background jobs
@@ -19,12 +18,6 @@ builder.Services.AddBusinessServices();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
