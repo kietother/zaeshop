579cb54b179fcea990dcd9287bc12ef3ddaf2cf0
kietdev1 <124799849+kietdev1@users.noreply.github.com>
2024-01-29 10:32:59 +0000

    Update Calculate Views Stored

--- src/Services/Portal/Portal.API/App_Data/StoredProcedures/Collection_Album_RecalculateViews.sql
+++ src/Services/Portal/Portal.API/App_Data/StoredProcedures/Collection_Album_RecalculateViews.sql
@@ -3,6 +3,7 @@ CREATE OR ALTER PROCEDURE Collection_Album_RecalculateViews (
 )
 AS
 BEGIN
+	-- Update Views for Collections (Chapters)
 	SELECT
 		c.Id,
 		SUM(cv.[View] )AS [Views]
@@ -17,13 +18,19 @@ BEGIN
 	FROM dbo.Collection c
 		JOIN #viewCollections vc ON vc.Id = c.Id
 
+	-- Update Views for Albums (Comics)
+	-- Calculate views for album from updated collection
 	SELECT 
 		a.Id,
 		SUM(c.[Views]) AS [Views]
 	INTO #viewAlbums
 	FROM dbo.Album a
 		JOIN dbo.Collection c ON c.AlbumId = a.Id
-	WHERE EXISTS (SELECT value FROM STRING_SPLIT(@collectionIds, ',') WHERE value = c.Id)
+	WHERE EXISTS (
+		SELECT 1
+		FROM dbo.Collection c2
+		WHERE EXISTS (SELECT value FROM STRING_SPLIT(@collectionIds, ',') WHERE value = c2.Id) AND a.Id = c2.AlbumId
+	)
 	GROUP BY a.Id
 
 	UPDATE a
