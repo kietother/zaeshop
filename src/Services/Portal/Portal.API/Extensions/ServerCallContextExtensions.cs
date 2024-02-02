aea5277c52f19df6f7599958a9400e6464a8d045
kietdev1 <124799849+kietdev1@users.noreply.github.com>
2024-02-02 03:43:20 +0000

    Update code

--- src/Services/Portal/Portal.API/Extensions/ServerCallContextExtensions.cs
+++ src/Services/Portal/Portal.API/Extensions/ServerCallContextExtensions.cs
@@ -10,7 +10,7 @@ namespace Portal.API.Extensions
         {
             bool isDeployed = bool.Parse(Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT_DEPLOYED") ?? "false");
             return (!isDeployed && (context.Host.Contains("localhost") || context.Host.Contains("127.0.0.1")))
-                || context.Host.Contains(CommonHelper.GetServiceUrl(EServiceHost.Portal), StringComparison.OrdinalIgnoreCase);
+                || CommonHelper.GetServiceUrl(EServiceHost.Portal).Contains(context.Host, StringComparison.OrdinalIgnoreCase);
         }
     }
 }
