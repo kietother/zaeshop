625098f2c8ec3732a5f64e1a22d4e73887f85cdf
kietdev1 <124799849+kietdev1@users.noreply.github.com>
2024-02-02 02:25:42 +0000

    Update UI

--- lib/services/client/auth/index.ts
+++ lib/services/client/auth/index.ts
@@ -5,12 +5,13 @@ import UserSession from "@/app/models/auth/UserSession";
 
 export const getTokenFromSessionServer = async () => {
     const response = await axiosClientApiInstance.get<ClientTokenSessionServer>(clientServer + '/api/session');
-    if (response.status == 200 && response.data && response.data.session) {
+    if (response.status == 200 && response.data?.session) {
         const apiToken = response.data.session?.user.token?.apiToken ?? '';
         const userSession: UserSession = {
             name: response.data.session?.user.name,
             email: response.data.session?.user.email,
             image: response.data.session?.user.image,
+            roles: response.data.session?.user.token?.roles
         }
 
         localStorage.setItem('token', apiToken);
