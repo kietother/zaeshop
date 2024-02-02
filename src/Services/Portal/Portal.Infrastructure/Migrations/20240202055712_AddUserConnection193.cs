289e7c2dfc90a4fa39d6e18a417ca289a014833b
kietdev1 <124799849+kietdev1@users.noreply.github.com>
2024-02-02 13:12:42 +0700

    193 Add UserConnection (#203)
    * 193: Add User Connection
    
    * 193: Add migration

---
+++ src/Services/Portal/Portal.Infrastructure/Migrations/20240202055712_AddUserConnection193.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Portal.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserConnection193 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserConnection",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    ConnectionId = table.Column<string>(type: "VARCHAR(100)", nullable: false),
+                    CreatedOnUtc = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedOnUtc = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserConnection", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserConnection_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserConnection_ConnectionId",
+                table: "UserConnection",
+                column: "ConnectionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserConnection_UserId",
+                table: "UserConnection",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserConnection");
+        }
+    }
+}
