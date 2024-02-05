8d5b6b55912cda958af94047d54d010750058812
kietdev1 <124799849+kietdev1@users.noreply.github.com>
2024-02-05 09:08:08 +0000

    174: Add Level Entity

---
+++ src/Services/Portal/Portal.Infrastructure/EntityConfigurations/UserAggregate/UserLevelEntityTypeConfiguration.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Portal.Domain.AggregatesModel.UserAggregate;
+
+namespace Portal.Infrastructure.EntityConfigurations.UserAggregate
+{
+    public class UserLevelEntityTypeConfiguration : IEntityTypeConfiguration<UserLevel>
+    {
+        public void Configure(EntityTypeBuilder<UserLevel> builder)
+        {
+            builder.ToTable(nameof(Level));
+            builder.HasKey(x => x.Id);
+
+            builder.HasOne(x => x.Level).WithMany(y => y.UserLevels).HasForeignKey(z => z.LevelId);
+            builder.HasOne(x => x.User).WithMany(y => y.UserLevels).HasForeignKey(z => z.UserId);
+        }
+    }
+}
