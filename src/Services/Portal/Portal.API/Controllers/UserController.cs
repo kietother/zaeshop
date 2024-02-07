69a7e509e46bd7a93e5bffbcbe4e0c27172c4834
kietdev1 <124799849+kietdev1@users.noreply.github.com>
2024-02-07 20:53:04 +0700

    174: Return Exp From User (#209)
    * 174: Implement Logic User Earn Exps
    
    * 174: Add Background into API Comic and Comment
    
    * 174: Add stored
    
    * 174: Update code
    
    * 174: Return Exp From User

---
+++ src/Services/Portal/Portal.API/Controllers/UserController.cs
@@ -0,0 +1,53 @@
+using Common.Models;
+using Microsoft.AspNetCore.Mvc;
+using Portal.API.Attributes;
+using Portal.Domain.AggregatesModel.UserAggregate;
+using Portal.Domain.Models.UserModels;
+
+namespace Portal.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class UserController : BaseApiController
+    {
+        private readonly IGenericRepository<User> _userRepository;
+
+        public UserController(IUnitOfWork unitOfWork)
+        {
+            _userRepository = unitOfWork.Repository<User>();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProfile()
+        {
+            var identityUserId = GetIdentityUserIdByToken();
+            if (string.IsNullOrEmpty(identityUserId))
+            {
+                return BadRequest("error_user_not_found");
+            }
+
+            var user = await _userRepository.GetByIdentityUserIdAsync(identityUserId);
+            if (user == null)
+            {
+                return BadRequest("error_user_not_found");
+            }
+
+            // Response
+            var response = new UserProfileResponse
+            {
+                Id = user.Id,
+                Email = user.Email,
+                Avatar = user.Avatar,
+                FullName = user.FullName,
+                UserName = user.UserName,
+                LevelId = user.LevelId,
+                CurrentExp = user.CurrentExp,
+                NextLevelExp = user.NextLevelExp,
+                RoleType = user.RoleType
+            };
+
+            return Ok(new ServiceResponse<UserProfileResponse>(response));
+        }
+    }
+}
