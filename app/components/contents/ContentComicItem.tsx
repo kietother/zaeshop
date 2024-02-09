b22ff02b3bda024fcf136ac9f67961972e2c86ed
kietdev1 <124799849+kietdev1@users.noreply.github.com>
2024-02-09 02:24:40 +0000

    Improve UX Lazy Image

--- app/components/contents/ContentComicItem.tsx
+++ app/components/contents/ContentComicItem.tsx
@@ -1,14 +1,17 @@
 "use client"
 import React from 'react';
 import { LazyLoadImage, trackWindowScroll } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 
 export function ContentComicItem({ imageUrl, scrollPosition }: { imageUrl: string, scrollPosition: any }) {
     return (
         <div className="chapter-image col-lg-10 offset-lg-1 col-12 offset-0 img-chapter">
-            <LazyLoadImage src={imageUrl} 
+            <LazyLoadImage src={imageUrl}
+                effect="blur"
                 alt=""
                 scrollPosition={scrollPosition}
                 width={800}
+                threshold={200}
             />
         </div>
     )
