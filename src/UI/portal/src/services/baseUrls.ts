b047726e47bc018881c555fe45992d3eb4e20481
kietdev1 <124799849+kietdev1@users.noreply.github.com>
2024-02-11 06:08:06 +0000

    217: Update code

--- src/UI/portal/src/services/baseUrls.ts
+++ src/UI/portal/src/services/baseUrls.ts
@@ -1,4 +1,2 @@
 export const identityServer = 'https://codegota.me/identity';
-// export const identityServer = 'http://127.0.0.1:5291';
-// export const portalServer = 'http://127.0.0.1:5148';
 export const portalServer = 'https://codegota.me/portal';
