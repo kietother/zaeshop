8f46eb5944f4dd5bd57d5f597310ce1deedb7e2f
kietdev1 <124799849+kietdev1@users.noreply.github.com>
2024-02-11 03:38:20 +0000

    217: Add Filter Sort Album Paging

--- src/UI/portal/src/models/album/AlbumPagingRequest.ts
+++ src/UI/portal/src/models/album/AlbumPagingRequest.ts
@@ -1,4 +1,6 @@
 import PagingRequest from "../common/PagingRequest";
 
 export default interface AlbumPagingRequest extends PagingRequest {
+    topType?: string | null;
+    region?: string | null;
 }
