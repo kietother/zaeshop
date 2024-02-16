1ad4bc00ff975fee5509c8fe0b29df1e95f50c84
khoiNDD <152581970+khoiNDD@users.noreply.github.com>
2024-02-16 15:12:39 +0700

    Add api send mail (#229)

---
+++ src/Services/Portal/Portal.API/Controllers/EmailController.cs
@@ -0,0 +1,37 @@
+﻿using Common.Interfaces.Messaging;
+using Common.Shared.Models.Emails;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Portal.API.Attributes;
+
+namespace Portal.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class EmailController : BaseApiController
+    {
+        private readonly ISendMailPublisher _sendMailPublisher;
+
+        public EmailController(ISendMailPublisher sendMailPublisher)
+        {
+            _sendMailPublisher = sendMailPublisher;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SendMailAsync([FromBody] SendEmailMessage request)
+        {
+            var toEmail = string.Join(", ", request.ToEmails);
+            var ccEmail = string.Join(", ", request.CcEmails ?? new List<string>());
+            var message = new SendEmailMessage
+            {
+                Subject = request.Subject,
+                Body = request.Body,
+                ToEmails = toEmail!.Split(',').ToList(),
+                CcEmails = ccEmail?.Split(',').ToList()
+            };
+            await _sendMailPublisher.SendMailAsync(message);
+            return Ok();
+        }
+    }
+}
