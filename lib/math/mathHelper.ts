b8ca26df0a41a61437485f53d9c795b5bd6b6262
Ngô Đặng Đồng Khởi <47884180+DongKhoi@users.noreply.github.com>
2024-02-21 09:34:22 -0800

    Optimize ux (#114)
    * fix btn
    
    * Fix btn
    
    * Fix btn
    
    * fix btn
    
    * Fix login from mes
    
    * Fix btn login
    
    * hot fix
    
    * hot fix
    
    * add following
    
    * fix btn
    
    * Fix following
    
    * history reading
    
    * create upgrade page
    
    * Fix UI and region field
    
    * Hot fix
    
    * hot fix
    
    * Hot fix
    
    * Hot fix
    
    * hot fix
    
    * hot fix
    
    * Fix UI
    
    * Update
    
    * fix role
    
    * hot fix
    
    * Update UI
    
    * Clean code
    
    * Fix failure build
    
    * Clean code
    
    * fix aff link
    
    * Fix UI and SEO h1
    
    * Update UI testing
    
    * fix ui
    
    * Fix UI
    
    * fix paging responsive
    
    * Fix comment limit
    
    * hot fix
    
    * Fix payment send email
    
    * Fix comment request
    
    * Fix image
    
    * Fix ui package
    
    * Clean code
    
    * Fix payment
    
    * Fix log payment
    
    * Fix display
    
    * Hot fix
    
    * fix display
    
    * Fix logic comment
    
    * Fix level percent
    
    * Fix percent
    
    ---------
    
    Co-authored-by: khoingo99 <khoi.ngo@logixtek.com>
    Co-authored-by: khoiNDD <Ngoddk@carmd.com>

--- lib/math/mathHelper.ts
+++ lib/math/mathHelper.ts
@@ -1,5 +1,5 @@
 export const getPercentByDivdeTwoNumber = (number1?: number | null, number2?: number | null) => {
     if (!number1 || !number2) return 0;
     if (number2 === 0) return 0;
-    return Math.ceil((number1 - ( number2 - 1000) / 2 ) / ( number2 - number1 ) * 100);
+    return Math.ceil((number1 - ( number2 - 1000) / 2 ) / (( number2 + 1000 ) / 2) * 100);
 }
