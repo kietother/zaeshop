47e54c9654a08ab8d73b77ec962a74bb744bd8f9
Kiet Tran <44864496+kiettran99@users.noreply.github.com>
2024-02-22 13:02:04 +0000

    Update token 60

--- src/Services/Identity/Identity.Domain/Interfaces/Infrastructure/IJwtService.cs
+++ src/Services/Identity/Identity.Domain/Interfaces/Infrastructure/IJwtService.cs
@@ -5,7 +5,7 @@ namespace Identity.Domain.Interfaces.Infrastructure
 {
     public interface IJwtService
     {
-        string GenerateJwtToken(User user, int expirationInMinutes = 15);
+        string GenerateJwtToken(User user, int expirationInMinutes = 60);
         UserInfomationTokenModel? ValidateJwtToken(string token);
         UserToken GenerateRefreshToken(string ipAddress);
     }
